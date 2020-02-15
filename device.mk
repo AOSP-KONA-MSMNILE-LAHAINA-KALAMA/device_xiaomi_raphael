b45ed8eb24b8a6c3ec6c704df2a8780d7f7f8934
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-14 22:39:33 -0500

    raphael: Add custom audio amplifier for TFA amp feedback
     * Based on observing reverse engineering from davinci stock audio HAL.
     * Fixes broken stereo sound.
    
     * Same logic with Smosia, rewritten by me.
    
    Co-authored-by: Smosia <stasenko_nv@nrcki.ru>
    Change-Id: I369555a9fcb06065177636b436d2e006c7a0b07d

--- device.mk
+++ device.mk
@@ -93,6 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
+    audio_amplifier.msmnile \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msmnile \
